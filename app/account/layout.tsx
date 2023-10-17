e6f57f5fa6387158bbee33c4f701967d2c7557fc
Jaydip Bhingradiya <jbhingradiya03@gmail.com>
2023-10-17 09:57:40 +0530

    changes on account and login layout

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -3,9 +3,9 @@
 import { AxiosError } from 'axios';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import Header from '@/components/common/Header';
-import Footer from '@/components/common/Footer';
-import Loading from '@/components/common/Loading';
+// import Header from '@/components/common/Header';
+// import Footer from '@/components/common/Footer';
+// import Loading from '@/components/common/Loading';
 import { useSelector } from 'react-redux';
 import get from '@/lib/requests';
 
@@ -35,24 +35,22 @@ export default function DashboardLayout({
 		})();
 	}, [push]);
 
-	if (!isSuccess) {
-		return (
-			<Loading
-				width={50}
-				height={50}
-				className="flex absolute justify-center w-96
-			z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
-			/>
-		);
-	}
+	// if (!isSuccess) {
+	// 	return (
+	// 		<Loading
+	// 			width={50}
+	// 			height={50}
+	// 			className="flex absolute justify-center w-96
+	// 		z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
+	// 		/>
+	// 	);
+	// }
 	return (
 		<div className="min-h-screen">
-			<Header />
+			{/* <Header /> */}
 			{children}
 			<div className="w-full bg-[#151515]">
-				<div className="max-w-7xl mx-auto py-20">
-					<Footer />
-				</div>
+				<div className="max-w-7xl mx-auto py-20">{/* <Footer /> */}</div>
 			</div>
 		</div>
 	);
