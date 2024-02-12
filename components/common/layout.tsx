6b9c45fdcb3f20811cdd1d374657bc0ffc98bc40
eric <eric@vibeground.com>
2024-02-12 17:18:17 +0200

    WEB-29

--- components/common/layout.tsx
+++ components/common/layout.tsx
@@ -15,3 +15,46 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 		</PageWrapper>
 	);
 }
+
+export function Address(props: { showEmail: boolean; }) {
+	return (
+		<p
+			style={{
+				marginTop: '14pt',
+				marginBottom: '14pt',
+				lineHeight: 'normal',
+			}}>
+			<strong>
+				<span style={{ fontFamily: '"Times New Roman"' }}>Vibeground Ltd.</span>
+			</strong>
+			<br />
+			<strong>
+				<span style={{ fontFamily: '"Times New Roman"' }}>
+					Persefonis 8
+				</span>
+			</strong>
+			<br />
+			<strong>
+				<span style={{ fontFamily: '"Times New Roman"' }}>
+					2102 Nicosia
+				</span>
+			</strong>
+			<br />
+			<strong>
+				<span style={{ fontFamily: '"Times New Roman"' }}>
+					Cyprus
+				</span>
+			</strong>
+			<br />
+			{props.showEmail ?
+				<a href="mailto:info@vibeground.com" style={{ textDecoration: 'none' }}>
+					<span
+						className="Hyperlink"
+						style={{ fontFamily: '"Times New Roman"', fontWeight: 'bold' }}>
+						info@vibeground.com
+					</span>
+				</a>
+			: null}
+		</p>
+	);
+}
